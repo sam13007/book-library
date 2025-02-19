a88ea6601e8f4d4afa4263f2a4c607a49a292de1
sam13007 <shyam13007@gmail.com>
2025-02-19 23:39:02 +0530

    fix: resolvers in graphql

---
+++ backend/src/graphql/resolvers.ts
@@ -0,0 +1,21 @@
+import { IResolvers } from "@graphql-tools/utils";
+import Book, { IBook } from "../model/Book";
+
+const resolvers = {
+  Query: {
+    books: async (): Promise<IBook[]> => await Book.find(),
+    book: async (_: any, { id }: { id: string }): Promise<IBook | null> =>
+      await Book.findById(id),
+  },
+  Mutation: {
+    addBook: async (
+      _: any,
+      { title, author }: { title: string; author: string }
+    ) => {
+      const newBook = new Book({ title, author });
+      return await newBook.save();
+    },
+  },
+};
+
+export default resolvers;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,14 @@
 import connectDB from "./config/database";
+import { ApolloServer } from "apollo-server";
+import dotenv from "dotenv";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
 
+dotenv.config();
 connectDB();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
